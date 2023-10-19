22523f6950d2842f587604deea6abc44da043220
Jørgen Moseid Ulven <jorgenulven@gmail.com>
2023-10-19 22:59:21 +0200

    Replace elements
    Replace elements for a button with events
    
    #2

--- FirstGUI/MainFrame.cpp
+++ FirstGUI/MainFrame.cpp
@@ -1,36 +1,24 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
-#include <wx/spinctrl.h>
+
+enum IDs {
+	BUTTON_ID = 2
+};
+
+
+wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
+	EVT_BUTTON(BUTTON_ID, MainFrame::OnButtonClicked)
+wxEND_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35), wxBU_LEFT);
+	wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 275), wxSize(200, 50));
 
-	wxCheckBox* checkBox = new wxCheckBox(panel, wxID_ANY, "CheckBox", wxPoint(550, 55), wxDefaultSize, wxCHK_3STATE | wxCHK_ALLOW_3RD_STATE_FOR_USER);
+	CreateStatusBar();
 
-	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "StaticText - Not editable", wxPoint(0, 150), wxSize(400, -1), wxALIGN_CENTER_HORIZONTAL);
-	staticText->SetBackgroundColour(*wxLIGHT_GREY);
+}
 
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY, "TextCtrl - editable", wxPoint(500, 145), wxSize(200, -1),
-	wxTE_PASSWORD);
-
-	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1), wxSL_VALUE_LABEL);
-
-	wxGauge* gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(590, 205), wxSize(-1, 125), wxGA_VERTICAL);
-	gauge->SetValue(50);
-
-	wxArrayString choices;
-	choices.Add("Item B");
-	choices.Add("Item C");
-	choices.Add("Item A");
-
-	wxChoice* choice = new wxChoice(panel, wxID_ANY, wxPoint(150, 375), wxSize(100, -1), choices, wxCB_SORT);
-	choice->Select(0);
-
-	wxSpinCtrl* spinCtrl = new wxSpinCtrl(panel, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1), wxSP_WRAP);
-
-	wxListBox* listBox = new wxListBox(panel, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices, wxLB_MULTIPLE);
-
-	wxRadioBox* radibox = new wxRadioBox(panel, wxID_ANY, "RadioBox", wxPoint(550, 475), wxDefaultSize, choices, 1, wxRA_SPECIFY_ROWS);
+void MainFrame::OnButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Button Clicked");
 }
